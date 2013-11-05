45dfc0c30dd1e05ccaab769e67e75bf3986946b8
Pavel Dohnal <pavel@dohnal.us>
2013-11-05 08:48:18 +0100

    sign in/up switching

--- www/js/main.js
+++ www/js/main.js
@@ -1,3 +1,11 @@
-$(function(){
+function LoginCtrl($scope) {
+	$scope.signinVisible = true;
 
-});
+	$scope.showSignIn = function () {
+		$scope.signinVisible = true;
+	}
+
+	$scope.showSignUp = function () {
+		$scope.signinVisible = false;
+	}
+}
