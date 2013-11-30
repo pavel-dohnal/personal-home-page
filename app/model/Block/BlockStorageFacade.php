dc6b6b060c9903768b21986566c91c377f9411b6
Pavel Dohnal <pavel@dohnal.us>
2013-11-30 17:05:10 +0100

    block storage facade empty file created

--- app/model/Block/BlockStorageFacade.php
+++ app/model/Block/BlockStorageFacade.php
@@ -1,2 +1,23 @@
 <?php
-//TODO
+
+namespace Block;
+
+class StorageFacade
+{
+
+	/**
+	 * @var \Doctrine\ORM\EntityManager
+	 */
+	private $entityManager;
+
+	public function __construct(\Doctrine\ORM\EntityManager $entityManager)
+	{
+		$this->entityManager = $entityManager;
+	}
+
+	public function create()
+	{
+		
+	}
+
+}
