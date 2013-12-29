6b73ef4da7f3f14bc0eeca2fcb7b7aceee6f5322
Pavel Dohnal <pavel@dohnal.us>
2013-12-29 21:20:44 +0100

    CreateBlockController structure

--- app/model/Block/CreateBlockController.php
+++ app/model/Block/CreateBlockController.php
@@ -4,9 +4,18 @@ namespace Block;
 
 class CreateBlockController
 {
+	//dependencies:
+	//validatorFactory
+	//createBlockFacadeFactory
+
 	public function run(\User\Entity\User $user, \stdClass $input = null)
 	{
-		//TODO
+		//validate input
+		//in createBlockFacade
+			//load users block
+			//if user block not found, create new
+			//create block
+			//flush
 		return [];
 	}
 }
